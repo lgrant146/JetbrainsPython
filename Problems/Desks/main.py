3f508f15ef342fa43b094a3162b0091dd2d393b8
Leroy Grant <lgrant146@gmail.com>
2020-09-14 18:15:53 -0500

    program with numbers  done

---
+++ Problems/Desks/main.py
@@ -0,0 +1,18 @@
+# put your python code here
+
+
+group1 = int(input())
+
+group2 = int(input())
+
+group3 = int(input())
+
+desk1 = group1 // 2 + group1 % 2
+
+desk2 = group2 // 2 + group2 % 2
+
+desk3 = group3 // 2 + group3 % 2
+
+total_desks = desk1 + desk2 + desk3
+
+print(total_desks)
---
+++ Problems/How many nuts will be left after division/main.py
@@ -0,0 +1,7 @@
+# put your python code here
+
+N = int(input())
+
+K = int(input())
+
+print(K % N)
